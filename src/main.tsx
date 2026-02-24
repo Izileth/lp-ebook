53543d21546b5f3f0443da9ec6df41f50eaa3163
Kawã Kleber <kawaklebersc@gmail.com>
2026-02-24 19:32:39 -0300

    add main router

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Router } from './router'
-import { AuthProvider } from './hooks/useAuth';
+//import { AuthProvider } from './hooks/useAuth';
 //import { UserProfileProvider } from './hooks/useUserProfile';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
         <Router />
-    </AuthProvider>
   </StrictMode>,
 )
